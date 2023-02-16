5221abe85fc877aaafb117909a01fdebdabfb6ef
cristhianguelrithw <123845589+cristhianguelrith@users.noreply.github.com>
2023-02-16 12:05:52 -0300

    Edit 11

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import ReadAll from './components/ReadAll/ReadAll'
 
 function App() {
   return (
     <div className="App">
-      Hello, World JSX!
+      <ReadAll />
     </div>
   )
 }
