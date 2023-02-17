60dc6a7a4b00495d49ce73376f5241fa637a4000
cristhianguelrithw <123845589+cristhianguelrith@users.noreply.github.com>
2023-02-17 12:00:03 -0300

    Edit 13

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,8 +1,12 @@
-function Card() {
+import "./Card.css"
+
+function Card(props) {
+    console.log(props)
+    const item = props.item;
     return (
         <div className="Card">
-            <h1>Phineas e Ferb</h1>
-            <img src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/01/13/2089303233-phineas-e-ferb.jpg" className="imagephoto" alt="" srcset="" />
+            <h1>{item.nome}</h1>
+            <img src={item.imageUrl} className="imagephoto" alt="" srcSet="" />
         </div>
     )
 }
--- frontend/src/components/ReadAll/ReadAll.jsx
+++ frontend/src/components/ReadAll/ReadAll.jsx
@@ -1,25 +1,30 @@
-/**
- * Exercício 1:
- * Dentro do componente ReadAll, crie um div com a classe "Card"
- * Dentro do Card, exiba uma imagem de qualquer personagem
- * Além disso, tb exiba o nome da parsonagem
- * 
- * OBS: caso vc tenha escolhido algo diferente no backend, pode manter o tema original
- * Não precisa ser integrado com o Back! Apenas a exibição dos dados no Front.
- * 
- * Exercício 2 (bônus!):
- * Pegue o div com a classe "Card" e extraia em um componente JSX, seguindo os mesmos
- * passos que fizemos para a criação do componente ReadAll
- */
-
 import Card from"../Card/Card";
+import "./ReadAll.css"
+
+
+
+const items = [
+    {
+        _id: '1234',
+        nome: 'Phineas e Ferb',
+        imageUrl: 'https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/01/13/2089303233-phineas-e-ferb.jpg'
+    },
+    {
+        _id: '12345',
+        nome: 'Finn e Jake',
+        imageUrl: 'https://www.clipartmax.com/png/middle/40-407523_adventure-time-finn-and-jake-by-legaluslex-on-deviantart-adventure-time-finn.png'
+    },
+];
+
+
 
 function ReadAll() {
-    return (
-        <div className="Card">
-            <Card />
+    return <div className="ReadAll">
+            {items.map(function (item) {
+                {console.log(item)};
+                return <Card key={'card-' + item._id} item={item} />;
+            })}
         </div>
-    )
 }
 
 export default ReadAll;
