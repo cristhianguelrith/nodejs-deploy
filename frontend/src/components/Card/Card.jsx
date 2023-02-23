5a7ab537c5bfb77974fa62c538649959f923c4ee
cristhianguelrithw <123845589+cristhianguelrith@users.noreply.github.com>
2023-02-22 22:39:56 -0300

    Edit 15 - Atividades

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,12 +1,14 @@
 import "./Card.css"
 
 function Card(props) {
-    console.log(props)
+    // console.log(props)
     const item = props.item;
+    const tags = props.tags;
     return (
         <div className="Card">
             <h1>{item.nome}</h1>
             <img src={item.imageUrl} className="imagephoto" alt="" srcSet="" />
+            <h2>{tags.nome}</h2>
         </div>
     )
 }
--- frontend/src/components/ReadAll/ReadAll.jsx
+++ frontend/src/components/ReadAll/ReadAll.jsx
@@ -1,40 +1,68 @@
 import Card from"../Card/Card";
 import "./ReadAll.css"
 
-
+// Mock Items (exatamente a mesma estrutura que o back traria)
+// Isso facilita o trabalho aqui no Front, para conseguir estruturar
+// comportamento, sem precisar depender do back para receber dados
 
 const items = [
     {
         _id: '1234',
         nome: 'Phineas e Ferb',
-        imageUrl: 'https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/01/13/2089303233-phineas-e-ferb.jpg'
+        imageUrl: 'https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/01/13/2089303233-phineas-e-ferb.jpg',
+        const: tags = [
+            {
+                nome: 'Phineas e Ferb'
+            },
+            {
+                nome: 'DisneyXD'
+            },
+            {
+                nome: 'Desenho Animado'
+            },
+        ]
     },
     {
         _id: '12345',
         nome: 'Finn e Jake',
-        imageUrl: 'https://www.clipartmax.com/png/middle/40-407523_adventure-time-finn-and-jake-by-legaluslex-on-deviantart-adventure-time-finn.png'
+        imageUrl: 'https://www.clipartmax.com/png/middle/40-407523_adventure-time-finn-and-jake-by-legaluslex-on-deviantart-adventure-time-finn.png',
+        tag: 'Hora de Aventura'
     },
     {
         _id: '123456',
-        nome: 'Dipper e Mabel Pines',
-        imageUrl: 'https://super.abril.com.br/wp-content/uploads/2016/10/super_imggravity_falls_0_0.jpg?quality=90&strip=info&w=1024&resize=1200,800'
+        nome: 'Dipper e Mabel',
+        imageUrl: 'https://i.pinimg.com/474x/2c/0b/ba/2c0bbaf629ce5a77b324072edede5cef--dipper-and-mabel-mabel-pines.jpg',
+        tag: 'Gravity Falls'
     },
     {
         _id: '1234567',
         nome: 'Goku e Vegeta',
-        imageUrl: 'https://kanto.legiaodosherois.com.br/w728-h381-gnw-cfill-gcc/wp-content/uploads/2022/05/legiao_eqoT4k3g8CUI.jpg.webp'
+        imageUrl: 'https://img.ifunny.co/images/bf0a76eeb429822bc7f0a7c540e4e4b442ee6af5c0cdcb6fe041c47cf2c4106f_1.jpg',
+        tag: 'Dragon Ball Series'
     },
 ];
 
+/*
+- Para cada item da lista, exibe um card do Rick Sanchez
+- Pegar a informação de cada item e enviar para o componente <Card />
+- A informação recebida no card deve ser exibida no console.log
+- Pegar cada parte da informação recebida e colocá-la nos elementos de HTML
+*/
 
+// Sintaxe de concatenação de variáveis dentro do JSX, chaves: {}
+// { VARIAVEL }
+// Concatenar = unir de forma lógica
+
+// Na verdade, as chaves representam que código JavaScript pode ser
+// inserido dentro delas
 
 function ReadAll() {
     return <div className="ReadAll">
-            {items.map(function (item) {
-                {console.log(item)};
-                return <Card key={'card-' + item._id} item={item} />;
-            })}
-        </div>
+        {items.map(function (item) {
+            // {console.log(item)};
+            return <Card key={'card-' + item._id} item={item} />;
+        })}
+    </div>
 }
 
 export default ReadAll;
